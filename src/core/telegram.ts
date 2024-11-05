1de3f0b4b3f168b3a8a28c7ee4c0635435b98bf2
a.shumakov <a.shumakov@nanagency.ru>
2024-11-05 17:41:03 +0700

    fix

--- src/core/telegram.ts
+++ src/core/telegram.ts
@@ -5,8 +5,5 @@ let webApp: iWebApp;
 console.log("tgInfo", Telegram.WebApp);
 // @ts-ignore
 webApp = Telegram.WebApp;
-webApp.setHeaderColor("#f1f4fd");
-webApp.setBackgroundColor("#fff");
-webApp.isClosingConfirmationEnabled = true;
 console.log(webApp.initData);
 export { webApp };
