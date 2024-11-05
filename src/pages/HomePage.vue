892dc624e080b1a2ba9053dff2bcd38f09ddaf64
a.shumakov <a.shumakov@nanagency.ru>
2024-11-05 17:34:54 +0700

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,10 +2,11 @@
 import { useRoute } from 'vue-router';
 import { webApp } from '@/core/telegram';
 
+webApp.expand()
 const route = useRoute();
 </script>
 <template>
-  <div class="font-bold p-5">
+  <div class="font-bold p-5 bg-white">
     id реферал:<span class="text-[red]">{{ route.query.id }}</span>
     <pre>{{ webApp }}</pre>
   </div>
