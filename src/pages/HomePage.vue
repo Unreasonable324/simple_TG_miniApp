11ac8b696045dfae5173c085447e8b71329d8a63
a.shumakov <a.shumakov@nanagency.ru>
2024-11-05 18:43:59 +0700

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,7 +7,7 @@ const route = useRoute();
 <template>
   <div class="font-bold p-5 bg-white">
     id реферал:<span class="text-[red]">{{ route.query.id }}</span>
-    <pre>{{ webApp.initDataUnsafe.user }}</pre>
+    <pre>{{ webApp }}</pre>
   </div>
 </template>
 <style></style>
