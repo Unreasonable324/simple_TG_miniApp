8d5c2ad184b4873c66f5ebaed56e27d5675ec769
a.shumakov <a.shumakov@nanagency.ru>
2024-11-06 13:55:37 +0700

    fix

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-
 // import type { LoginWidgetUser } from 'vue-tg';
-// import { LoginWidget } from 'vue-tg';
+import { LoginWidget } from 'vue-tg';
 
 const data = ref({});
 
-// function handleUserAuth(user: LoginWidgetUser) {
-//   data.value = user;
-// }
+function handleUserAuth(user:any) {
+  data.value = user;
+}
 </script>
 <template>
   <div class="bg-white">
-      <!-- <LoginWidget bot-username="@green_way_test_bot" @auth="handleUserAuth" /> -->
+      <LoginWidget bot-username="@green_way_test_bot" @auth="handleUserAuth" />
       <pre>{{ data }}</pre>
   </div>
 </template>
