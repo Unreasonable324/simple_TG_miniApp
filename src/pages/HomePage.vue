503c284d66d246b770b689c3776b98fa896fd8f8
a.shumakov <a.shumakov@nanagency.ru>
2024-11-06 17:45:18 +0700

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,6 +8,8 @@ const route = useRoute();
 <template>
   <div class="font-bold p-5 bg-white">
     id реферал:<span class="text-[red]">{{ route.query.id }}</span>
+    <br />
+    номер телефона:<span class="text-[red]">{{ route.query.phone_number }}</span>
     <!-- <pre>{{ webApp }}</pre> -->
   </div>
 </template>
