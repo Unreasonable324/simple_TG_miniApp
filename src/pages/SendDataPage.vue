b8120735a25575adb475cbdc917c1a3f6ec086de
a.shumakov <a.shumakov@nanagency.ru>
2024-11-07 16:08:29 +0700

    fix

--- src/pages/SendDataPage.vue
+++ src/pages/SendDataPage.vue
@@ -5,14 +5,14 @@ import { useWebApp } from 'vue-tg';
 const { sendData } = useWebApp();
 const text = ref("");
 const send = () => {
-  sendData(text.value);
+  sendData(JSON.stringify({ data: text.value, button_text: "web" }));
 };
 </script>
 <template>
   <div class="flex gap-3 flex-col py-2">
     <input type="text" v-model="text" class="border" placeholder="text" />
     <button class="bg-blue-200 px-3 rounded-lg" @click="send">send</button>
-    {{text}}
+    {{ text }}
   </div>
 </template>
 <style></style>
