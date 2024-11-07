7d9170d360f8fc53ddc6c3e18efe2fc4ec8529d8
a.shumakov <a.shumakov@nanagency.ru>
2024-11-07 16:31:04 +0700

    fix

--- src/pages/SendDataPage.vue
+++ src/pages/SendDataPage.vue
@@ -2,17 +2,29 @@
 import { ref } from 'vue';
 import { useWebApp } from 'vue-tg';
 
-const { sendData } = useWebApp();
-const text = ref("");
+const { initDataUnsafe } = useWebApp();
+
+const message = ref("");
 const send = () => {
-  sendData(JSON.stringify({ data: text.value, button_text: "web" }));
+  if (!initDataUnsafe?.user?.id) return;
+  fetch(`https://api.telegram.org/bot${import.meta.env.VITE_BASE_BOT_TOKEN}/test/sendMessage`, {
+    method: "POST",
+    body: {
+      parse_mode: "html",
+      chat_id: initDataUnsafe.user.id,
+      text: message.value,
+    },
+  });
+  //   } catch (e) {
+  // console.log(e);
+  //   }
 };
 </script>
 <template>
   <div class="flex gap-3 flex-col py-2">
-    <input type="text" v-model="text" class="border" placeholder="text" />
+    <input type="text" v-model="message" class="border" placeholder="text" />
     <button class="bg-blue-200 px-3 rounded-lg" @click="send">send</button>
-    {{ text }}
+    {{ message }}
   </div>
 </template>
 <style></style>
