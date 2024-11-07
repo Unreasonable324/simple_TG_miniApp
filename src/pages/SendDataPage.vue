084567e08b400254cf6650bae005f7545277a198
a.shumakov <a.shumakov@nanagency.ru>
2024-11-07 16:04:57 +0700

    fix

--- src/pages/SendDataPage.vue
+++ src/pages/SendDataPage.vue
@@ -12,6 +12,7 @@ const send = () => {
   <div class="flex gap-3 flex-col py-2">
     <input type="text" v-model="text" class="border" placeholder="text" />
     <button class="bg-blue-200 px-3 rounded-lg" @click="send">send</button>
+    {{text}}
   </div>
 </template>
 <style></style>
