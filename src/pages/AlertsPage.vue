fd55b83b069d9f6b57b6268da9a12e2eb255fb08
a.shumakov <a.shumakov@nanagency.ru>
2024-11-07 15:49:43 +0700

    fix

--- src/pages/AlertsPage.vue
+++ src/pages/AlertsPage.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-import { BackButton, MainButton, useWebAppPopup } from 'vue-tg';
+import { ref } from 'vue';
+import { BackButton, MainButton, Popup, useWebAppPopup } from 'vue-tg';
 
+const text = ref("");
 const { showAlert } = useWebAppPopup();
+function handlePopupClose(buttonId: string) {
+  text.value = buttonId;
+}
+const buttons = [
+  {
+    id: "ok",
+    type: "ok",
+    text: "ok",
+  },
+  {
+    id: "cancel",
+    type: "cancel",
+    text: "cancel",
+  },
+] as PopupButton[];
 </script>
 <template>
-  <div class="">
-    <MainButton text="главная кнопка" @click="showAlert('главная кнопка')"></MainButton>
+  <div class="py-3">
+    <button class="bg-blue-200 px-3 rounded-lg">alert</button>
+    <Popup message="Hello" @close="handlePopupClose" :buttons="buttons" />
+    {{ text }}
+    <MainButton text="главная кнопка" @click="showAlert('тык на главную')"></MainButton>
     <BackButton @click="$router.push('/')" />
   </div>
 </template>
