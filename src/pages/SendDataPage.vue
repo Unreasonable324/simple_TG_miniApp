8cc2ee039411cc4eafd92d4e4e21e2ab3df7e31a
a.shumakov <a.shumakov@nanagency.ru>
2024-11-08 16:29:45 +0700

    fix

--- src/pages/SendDataPage.vue
+++ src/pages/SendDataPage.vue
@@ -2,12 +2,20 @@
 import { ref } from 'vue';
 import { useWebApp } from 'vue-tg';
 
-const { initDataUnsafe } = useWebApp();
+const { initDataUnsafe, sendData } = useWebApp();
 
 const message = ref<unknown>("");
 const token = "7871921914:AAE1tqgOHPexqTzqOXrLHkunIckD7fpo0S0";
 const send = async () => {
   if (!initDataUnsafe?.user?.id) return;
+  sendData(
+    JSON.stringify({
+      parse_mode: "html",
+      chat_id: initDataUnsafe.user.id,
+      text: message.value,
+    })
+  );
+
   fetch(
     `https://api.telegram.org/bot${token}/sendMessage?text=${message.value}&chat_id=${initDataUnsafe.user.id}&parse_mode=html`,
     {
