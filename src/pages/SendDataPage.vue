a01d1eb9d01812ba850c8d53f1c809370a3bb21e
a.shumakov <a.shumakov@nanagency.ru>
2024-11-08 16:45:12 +0700

    fix

--- src/pages/SendDataPage.vue
+++ src/pages/SendDataPage.vue
@@ -8,22 +8,14 @@ const message = ref<unknown>("");
 const token = "7871921914:AAE1tqgOHPexqTzqOXrLHkunIckD7fpo0S0";
 const send = async () => {
   if (!initDataUnsafe?.user?.id) return;
-  sendData("123213213test");
-
   fetch(
     `https://api.telegram.org/bot${token}/sendMessage?text=${message.value}&chat_id=${initDataUnsafe.user.id}&parse_mode=html`,
     {
       method: "POST",
-      // body: JSON.stringify({
-      //   parse_mode: "html",
-      //   chat_id: initDataUnsafe.user.id,
-      //   text: message.value,
-      // }),
+
     }
   );
-  //   } catch (e) {
-  // console.log(e);
-  //   }
+
 };
 </script>
 <template>
