680f196f99e2ba6e7670fb402c41fd0070f63865
a.shumakov <a.shumakov@nanagency.ru>
2024-11-11 11:07:26 +0700

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,6 +7,11 @@ const route = useRoute();
 </script>
 <template>
   <div class="font-bold p-5 bg-white">
+  <div class="">
+  <pre>{{route.query}}</pre>
+  
+  
+  </div>
     id реферал:<span class="text-[red]">{{ route.query.id }}</span>
     <br />
     номер телефона:<span class="text-[red]">{{ route.query.phone_number }}</span>
